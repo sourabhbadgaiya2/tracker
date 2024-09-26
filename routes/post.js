257107ed0d40254480bbb89e6aa1fcf1cc6ddb92
sourabhbadgaiya2 <sourabhbadgaiya2@gmail.com>
2024-09-26 18:57:12 +0530

    updte ui

--- routes/post.js
+++ routes/post.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-const Post = require('../models/postSchema');
-const User = require('../models/userSchema');
+const PostSchema = require('../models/postSchema');
+const UserSchema = require('../models/userSchema');
 
 const { isLoggedIn } = require('../utils/auth.middile');
 
@@ -18,26 +18,61 @@ router.get('/createpost/:id', isLoggedIn, function (req, res, next) {
   });
 });
 
-router.post('/createpost/:id', isLoggedIn, async function (req, res, next) {
+router.post('/createpost', isLoggedIn, async function (req, res, next) {
   try {
-    const newPost = new Post(req.body);
-    newPost.user = req.params.id;
-    const post = await newPost.save();
+    const newPost = new PostSchema(req.body);
+    newPost.user = req.user._id;
+    await newPost.save();
 
-    const user = await User.findById(req.params.id);
-    user.posts.push(post._id);
-    await user.save();
-    res.redirect('/users/profile');
+    req.user.posts.push(newPost._id);
+    await req.user.save();
+
+    res.redirect('/posts/show');
   } catch (error) {
     next(error);
   }
 });
 router.get('/show', isLoggedIn, async function (req, res, next) {
-  const users = await Post.find();
+  // const users = await Post.find();
   res.render('show', {
     title: 'show Post',
-    user: req.user,
-    users,
+    user: await req.user.populate('posts'),
   });
 });
+router.get('/details', isLoggedIn, async function (req, res, next) {
+  res.render('details', {
+    title: 'show Post',
+    user: await req.user.populate('posts'),
+  });
+});
+router.get('/update/:id', isLoggedIn, async function (req, res, next) {
+  const post = await PostSchema.findById(req.params.id);
+  res.render('update', {
+    title: 'show Post',
+    user: req.user,
+    post,
+  });
+});
+
+router.post('/update/:id', isLoggedIn, async function (req, res, next) {
+  try {
+    await PostSchema.findByIdAndUpdate(req.params.id, req.body);
+    res.redirect('/posts/show');
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/delete/:id', isLoggedIn, async function (req, res, next) {
+  try {
+    const post = await PostSchema.findByIdAndDelete(req.params.id);
+
+    await req.user.posts.pull(post._id);
+    await req.user.save();
+    res.redirect('/posts/details');
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
